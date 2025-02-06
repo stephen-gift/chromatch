1e5b6c3eb05c0376c5724723fa2e63238eb736ae
Stephen <79639335+stephen-gift@users.noreply.github.com>
2025-02-06 01:19:07 +0100

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Layout from "@/components/Layout";
 import { Toaster } from "@/components/ui/sonner";
-import SoundManager from "@/components/SoundManager";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
